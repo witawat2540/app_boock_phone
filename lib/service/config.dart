68c2c5aeca3f6ee4d74086b242d3793f5e5e818e
witawatd <witawatd@gmail.com>
2021-05-19 21:31:45 +0700

    set flutter 2

--- lib/service/config.dart
+++ lib/service/config.dart
@@ -2,22 +2,22 @@ import 'dart:io';
 import 'package:http/http.dart' as http;
 
 class connect {
-    String url = "http://192.168.1.56:3000/";
+    String url = "http://192.168.20.88:3000/";
     var header = {HttpHeaders.contentTypeHeader: "application/json"};
 
   Future<http.Response> get(route) async {
-    return  http.get(url + route, headers: header);
+    return  http.get(Uri.parse(url + route), headers: header);
   }
 
     Future<http.Response> post(route,data) async {
-      return  http.post(url + route, headers: header,body: data);
+      return  http.post(Uri.parse(url + route), headers: header,body: data);
     }
 
     Future<http.Response> put(route,data) async {
-      return  http.put(url + route, headers: header,body: data);
+      return  http.put(Uri.parse(url + route), headers: header,body: data);
     }
 
     Future<http.Response> delete(route) async {
-      return  http.delete(url + route, headers: header);
+      return  http.delete(Uri.parse(url + route), headers: header);
     }
 }
